02d5337ffee668bf5a32e38a303f80b91831fd45
Spook <85762391+BurritoSuicide@users.noreply.github.com>
2025-02-24 23:34:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import csv
 import requests
-import time 
+import time
 from tqdm import tqdm
 import os, sys
 import utils, contextlib
+
+# Clean terminal output
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Function to query VirusTotal for SHA256 hashes
 def query_virustotal_hash(hash_value, api_keys):
@@ -40,7 +44,7 @@ def query_virustotal_hash(hash_value, api_keys):
 def query_virustotal(ip, api_keys):
     url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
     headers = {"x-apikey": api_keys['VirusTotal']}
-    
+
     try:        
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
@@ -57,6 +61,31 @@ def query_virustotal(ip, api_keys):
     except Exception as e:
         return {'ip': ip, 'virustotal_reputation': 'Error: ' + str(e)}
 
+# Function to query VirusTotal for remote hosts
+def query_virustotal_host(host, api_keys):
+    url = f"https://www.virustotal.com/api/v3/domains/{host}"
+    headers = {"x-apikey": api_keys['VirusTotal']}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            print(f"Successful API call for {host} to VirusTotal. Gathering info..")
+            data = response.json()
+            if 'data' in data and 'attributes' in data['data']:
+                analysis_results = data['data']['attributes'].get('last_analysis_stats', {})
+
+                return {
+                    'host': host,
+                    'virustotal_reputation': analysis_results
+                }
+            else:
+                print(f"No detailed analysis found for {host} on VirusTotal.")
+                return {'host': host, 'virustotal_reputation': 'No detailed analysis found'}
+        else:
+            print(f"Unsuccessful API call for {host} to VirusTotal. Check your API Key or the host.")
+            return {'host': host, 'virustotal_reputation': 'Error: ' + str(response.status_code)}
+    except Exception as e:
+        return {'host': host, 'virustotal_reputation': 'Error: ' + str(e)}
 # Function to query AbuseIPDB
 def query_abuseipdb(ip, api_keys):
     url = f"https://api.abuseipdb.com/api/v2/check"
@@ -92,7 +121,7 @@ def get_hash_reputations(hash_list, api_keys):
             results = []
             for hash_value in hash_list:
                 virustotal_data = query_virustotal_hash(hash_value, api_keys)
-                
+
                 result = {
                     'Hash': hash_value,
                     'VirusTotal Reputation': '\n'.join(
@@ -110,6 +139,31 @@ def get_hash_reputations(hash_list, api_keys):
 
 
 # Main function to handle IP list and query APIs
+def get_host_reputations(host_list, api_keys):
+    progress_bar = tqdm(total=len(host_list), desc="Processing Hosts", position=0, leave=True)
+    results = []
+    with contextlib.redirect_stdout(None):
+        for host in host_list:
+            virustotal_data = query_virustotal_host(host, api_keys)
+
+
+            result = {
+                'Host': host,
+                'VirusTotal Reputation': '\n'.join(
+                    f"{key}: {value}"
+                    for key, value in virustotal_data['virustotal_reputation'].items()
+                )
+                if isinstance(virustotal_data['virustotal_reputation'], dict)
+                else virustotal_data['virustotal_reputation'],
+                'AbuseIPDB Reputation': "Host Reputation cannot be queried via AbuseIPDB API."
+            }
+
+            progress_bar.update(1)
+            results.append(result)
+            time.sleep(15)
+
+    return results
+
 def get_ip_reputations(ip_list, api_keys):
     progress_bar = tqdm(total=len(ip_list), desc="Processing IPs", position=0, leave=True)
     results = []
@@ -118,7 +172,7 @@ def get_ip_reputations(ip_list, api_keys):
             virustotal_data = query_virustotal(ip, api_keys)
             abuseipdb_data = query_abuseipdb(ip, api_keys)
 
-            
+
             result = {
                 'IP': ip,
                 'VirusTotal Reputation': '\n'.join(
@@ -134,37 +188,100 @@ def get_ip_reputations(ip_list, api_keys):
                 if isinstance(abuseipdb_data['abuseipdb_reputation'], dict)
                 else abuseipdb_data['abuseipdb_reputation']
             }
-            
+
             progress_bar.update(1)
             results.append(result)
 
             time.sleep(15)  # Adding delay to avoid hitting rate limits of APIs
-    
+
 
     return results
 
 # Function to save results to CSV
-def save_to_csv(data, filename='output.csv'):
-    keys = data[0].keys()
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=keys)
-        writer.writeheader()
-        writer.writerows(data)
+def save_to_csv(data, filename='ip_output.csv'):
+    if data:
+        keys = data[0].keys()
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(data)
+
+# Function to save host results to CSV
+def save_host_to_csv(data, filename='host_output.csv'):
+    if data:
+        keys = data[0].keys()
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(data)
 
 # Read IP addresses from a text file
 def read_ip_list(file_path='ips.txt'):
     with open(file_path, 'r') as f:
         return [line.strip() for line in f.readlines()]
-
+    
 # Read SHA256 hashes from a text file
 def read_hash_list(file_path='hashes.txt'):
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             return [line.strip() for line in f.readlines()]
     return []
+
+def read_remote_host_list(file_path = 'hosts.txt'):
+    with open(file_path, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+def file_check():
+    files_to_check = ["ips.txt", "hashes.txt", "hosts.txt"]
+    for file_name in files_to_check:
+        if not os.path.exists(file_name):
+            clear_terminal()
+            print(f"File '{file_name}' not found.")
+            create_file = input(f"Would you like to create '{file_name}'? (yes/no): ").lower()
+            if create_file == "yes":
+                create_method = input("Would you like to copy and paste the contents or create it manually? (copy/manual): ").lower()
+                if create_method == "copy":
+                    # Copy and paste content
+                    print(f"Paste the content for '{file_name}' here (press Ctrl+D when done):")
+                    lines = []
+                    try:
+                        while True:
+                            line = input()
+                            lines.append(line)
+                    except EOFError:
+                        pass  # End of input reached
+                    content = '\n'.join(lines)
+                    with open(file_name, 'w') as f:
+                        try:
+                            f.write(content)
+                            print(f"File '{file_name}' created and content pasted.")
+                        except Exception as e:
+                            print(f"Error pasting content into '{file_name}': {e}")
+                elif create_method == "manual":
+                    # Create manually
+                    try:
+                        with open(file_name, 'w') as f:
+                            print(f"File '{file_name}' created.")
+                    except Exception as e:
+                        print(f"Error creating '{file_name}': {e}")
+
+                else:
+                    print("Invalid choice. File not created.")
+            else:
+                print(f"'{file_name}' not created.")
+            clear_terminal()
+            main_menu()
+
 # Main script execution
 if __name__ == "__main__":
-    print(f"""
+
+    time.sleep(2)
+    file_check()
+    clear_terminal()
+
+def main_menu():
+
+    print("""
  ::::::::  :::    :::  ::::::::   :::::::: ::::::::::: :::     ::: ::::::::::: ::::::::  :::::::::: 
 :+:    :+: :+:    :+: :+:    :+: :+:    :+:    :+:     :+:     :+:     :+:    :+:    :+: :+:        
 +:+        +:+    +:+ +:+    +:+ +:+           +:+     +:+     +:+     +:+    +:+        +:+        
@@ -172,47 +289,112 @@ if __name__ == "__main__":
 +#+   +#+# +#+    +#+ +#+    +#+        +#+    +#+      +#+   +#+      +#+    +#+        +#+        
 #+#    #+# #+#    #+# #+#    #+# #+#    #+#    #+#       #+#+#+#       #+#    #+#    #+# #+#        
  ########  ###    ###  ########   ########     ###         ###     ########### ########  ########## 
-
-                                          Initializing...
     """)
 
+    time.sleep(1)
+    print("Files Checked.")
+    print("ips.txt, hashes.txt, and hosts.txt have been checked for creation.")
+    print("Please ensure they are populated with the correct values you wish to query.")
     time.sleep(2)
+    print("Loading Main Menu...")
+    time.sleep(1)
 
-def main_menu():
+
     api_keys = utils.load_api_keys()  # Load API keys here
     if not api_keys:
-      print("No API keys found. Please set them first (Option 1).")
+        print("No API keys found. Please set them first (Option 1).")
     
     while True:
+        # Function to set the API key.
         def option_1():
             current_keys = utils.load_api_keys()
             if not current_keys:
                 print("No API keys found. You need to enter API keys for VirusTotal and AbuseIPDB")
             else:
                 print("\nCurrent API keys:")
-                print(f"  VirusTotal: {current_keys.get('VirusTotal', 'Not set')}")
+                print(f"  VirusTotal: {current_keys.get('VirusTotal', 'Not set')}") 
+
                 print(f"  AbuseIPDB: {current_keys.get('AbuseIPDB', 'Not set')}")
             print("\nEnter new API keys (or press Enter to skip):")
             new_virustotal_key = input("Enter new VirusTotal API Key: ")
             new_abuseipdb_key = input("Enter new AbuseIPDB API Key: ")
 
             updated_keys = {'VirusTotal': new_virustotal_key, 'AbuseIPDB': new_abuseipdb_key}
-            
+
             utils.save_api_keys(updated_keys)  # Save the updated keys, even if they're empty
             print("\nUpdated API keys, checking now.")
-            print(f"  VirusTotal: {utils.load_api_keys().get('VirusTotal','Not set')}")
+            print(f"  VirusTotal: {utils.load_api_keys().get('VirusTotal','Not set')}") 
+
             print(f"  AbuseIPDB: {utils.load_api_keys().get('AbuseIPDB','Not set')}")
             print("\nAPI keys saved.")
-        print("Main Menu:")
-        print("\n")
+
+        # Function to copy and paste IPs/Hashes/Hosts
+        def option_6():
+            print("\nChoose which file to paste into:")
+            print("1. ips.txt")
+            print("2. hashes.txt")
+            print("3. hosts.txt")
+            file_choice = input("Enter your choice (1-3): ")
+
+            if file_choice in ['1', '2', '3']:
+                if file_choice == '1':
+                    file_name = 'ips.txt'
+                elif file_choice == '2':
+                    file_name = 'hashes.txt'
+                else:
+                    file_name = 'hosts.txt'
+
+                print(f"Paste the content for '{file_name}' here (press Ctrl+D when done):")
+                lines = []
+                try:
+                    while True:
+                        line = input()
+                        lines.append(line)
+                except EOFError:
+                    pass  # End of input reached
+                content = '\n'.join(lines)
+                with open(file_name, 'w') as f:
+                    try:
+                        f.write(content)
+                        print(f"Content pasted into '{file_name}'.")
+                    except Exception as e:
+                        print(f"Error pasting content into '{file_name}': {e}")
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
+        #Function to delete and refresh files.
+        def option_7():
+            print("\nChoose which file to delete and refresh:")
+            print("1. ips.txt")
+            print("2. hashes.txt")
+            print("3. hosts.txt")
+            file_choice = input("Enter your choice (1-3): ")
+
+            if file_choice in ['1', '2', '3']:
+                if file_choice == '1':
+                    file_name = 'ips.txt'
+                elif file_choice == '2':
+                    file_name = 'hashes.txt'
+                else:
+                    file_name = 'hosts.txt'
+                if os.path.exists(file_name):
+                    os.remove(file_name)
+                    print(f"'{file_name}' has been deleted.")
+                    file_check()
+                else:
+                    print(f"'{file_name}' does not exist.")
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
+        print("\nMain Menu:")
         print("1. Set API Keys for VirusTotal & AbuseIPDB")
         print("2. Run Reputation Analysis for IPv4 Addresses")
         print("3. Run Repuation Analysis for SHA256 Hashes")
-        print("5. Run Repuation Analysis for Remote Hosts")
-        print("6. Extract Above Indicators from a text file.")
+        print("4. Run Repuation Analysis for Remote Hosts")
+        print("5. Extract Above Indicators from a text file.")
+        print("6. Copy & Paste IPs/Hashes/Hosts into respective files.")
+        print("7. Delete and Refresh Ips.txt, Hashes.txt, Hosts.txt")
         print("E. Exit")
 
-        choice = input("Enter your choice (1-7), or 'e' to exit: ")
+        choice = input("Enter your choice (1-7), or 'e' to exit: ").lower()
         api_keys = utils.load_api_keys()  # Load API keys here again for option 2
 
         if choice == '1':
@@ -220,13 +402,14 @@ def main_menu():
         elif choice == '2':
             ip_list = read_ip_list()
             if ip_list:
+                print("Gathering reputation report for IP addresses....")
                 print(f"Using the following VirusTotal API Key for IP Reputation Report: {api_keys['VirusTotal']}")
                 print(f"Using the following AbuseIPDB API Key for IP Reputation Report: {api_keys['AbuseIPDB']}")
                 results = get_ip_reputations(ip_list, api_keys)
-                save_to_csv(results)
-                print("Reputation report has been saved to output.csv")
+                save_to_csv(results)   
+                print("Reputation report has been saved to ip_output.csv")
             else:
-                print("No valid IP addresses found in ips.txt")
+                print("\nNo valid IP addresses found in ips.txt")
         elif choice == '3':
             hash_list = read_hash_list()
             if hash_list:
@@ -234,12 +417,28 @@ def main_menu():
                 save_to_csv(results, filename='hash_output.csv')
                 print("Reputation report for hashes has been saved to hash_output.csv")
             else:
-                print("No valid SHA256 hashes found in hashes.txt")
-            
+                print("\nNo valid SHA256 hashes found in hashes.txt")
+        elif choice == '4':
+            host_list = read_remote_host_list()
+            if host_list:
+                print("Gathering reputation report for remote hosts...")
+                results = get_host_reputations(host_list, api_keys)
+                save_host_to_csv(results)
+                print("Reputation report for hosts has been saved to host_output.csv")
+            else:
+                print("\nNo valid hosts found in hosts.txt")
+        elif choice == '5':
+            print("Extracting indicators from text file")
+        elif choice == '6':
+            option_6()
+        elif choice == '7':
+            option_7()
         elif choice == 'e':
             print("Exiting...")
+            clear_terminal()
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 main_menu()
